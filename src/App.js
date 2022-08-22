fac7dd609da270030b1337ed18a2186e4c088639
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-08-23 00:00:51 +0545

    learnt useRef

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 function App() {
     const [count, setCount] = useState(0);
+    // useRef is used as a reference to the static part without getElementById
+    const titleref = useRef();
+
+    const colors = ["black", "blue", "green", "red", "yellow", "white", "pink", "orange"]
 
     const incrementHandler = () => {
         setCount(count+1);
@@ -15,12 +19,13 @@ function App() {
     useEffect(() => {
         // useEffect runs on render and also when state of dependency changes eg. when count changes
         console.log("run with render");
-
+        titleref.current.style.color = colors[Math.floor(Math.random()*colors.length)];
     }, [count]);
 
     return (
         <div className="App">
             <div className="counter">
+                <h4 ref={titleref}>Learning Important Hooks</h4>
                 <h1>Count: {count}</h1>
                 <button className='btn' onClick={decrementHandler}>-1</button>
                 <button className='btn' onClick={incrementHandler}>+1</button>
