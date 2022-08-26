d401a0bdd0fafd99f3df3c74e6b5e5a0ed6a71c1
Sandesh Paudel <sandeshpaudel81@gmail.com>
2022-08-27 01:00:45 +0545

    setting up Routes with React router dom v6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
     return (
-        <Router>
-            <Route exact path='/'>
-                <Home/>
-            </Route>
-        </Router>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>} exact/>
+            </Routes>
+        </BrowserRouter>
     );
 }
 
